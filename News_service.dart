6431af10834af8042fa761b7fdb269a8db601707
Akashkumar-GitHub <123235927+Akashkumar-GitHub@users.noreply.github.com>
2025-06-04 00:13:53 +0530

    Update News_service.dart

--- News_service.dart
+++ News_service.dart
@@ -2,7 +2,7 @@ import 'dart:conert';
 import 'package:http/http.dart' as http;
 class NewsService{
   final String apikey = "bb54f6dc93ba4d68a2bb35d6efd4bfdd";
-  final String baseUrl = "https://newsapi.org/v2/top-headlines?country=us";
+  final String baseUrl = "https://newsapi.org/v2/top-headlines?country=us&apikey=bb54f6dc93ba4d68a2bb35d6efd4bfdd";
 
   Future<List<dynamic>> fetchNews() async {
     final response = await http.get(Uri.parse('$baseUrl$apiKey'));
